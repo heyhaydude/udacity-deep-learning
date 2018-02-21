b6e01ad814894bb530fd26f9f82bead0a2a60937
Matt Hayden <matt.hayden@mcmaster.com>
2018-02-21 12:39:41 -0600

    jupyter lab script

---
+++ _mamamatt/scripts/juplab.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# open jupyter notebooks for a project
+
+# run this command to give script execute permissions
+#       chmod +x ~/Desktop/DLJupyter.sh
+
+cd /Users/haydude/Development/udacity-deep-learning
+exec jupyter lab
